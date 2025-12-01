57f7ce604d5232b998d0c7978fb08745654df7ec
ellos0 <mcgibbonellott@gmail.com>
2025-11-30 18:40:58 -0800

    made emitC function

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import Lexer
+import Codegen
+
+emitC :: String -> [String]
+emitC xs = map codegenStatement (parseStringClean xs) 
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
