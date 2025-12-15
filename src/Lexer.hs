48bef26c9023c3fe46f24472d1bb1c2888284ea1
ellos0 <mcgibbonellott@gmail.com>
2025-12-14 21:42:25 -0800

    added dedicated lexing functions for defuns and assignments

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -2,6 +2,7 @@ module Lexer where
 
 import Text.Parsec (many1, letter, digit, oneOf, between, char, spaces, sepBy, ParseError, parse, try, (<|>))
 import Text.Parsec.String (Parser)
+
 import Structures
 
 atom :: Parser Expr
@@ -16,16 +17,22 @@ expression = try list <|> atom
 getAtoms :: String -> Either ParseError Expr
 getAtoms input = parse expression "" input
 
+lexDefun :: [Expr] -> Statement
+lexDefun xs = case (getAtomValue (xs !! 0)) of
+                (Just name) -> (Defun name (lexExpr (xs !! 1)) (lexExpr (xs !! 2)))
+                (Nothing) -> (Error (SyntaxError "Cannot name function using expression."))
+
+lexAssignment :: [Expr] -> Statement
+lexAssignment xs = case (getAtomValue (xs !! 0)) of
+                     (Just name) -> (Assignment name (lexExpr (xs !! 1)) (lexExpr (xs !! 2)))
+                     (Nothing) -> (Error (SyntaxError "Cannot name variable using expression."))
+
 lexExpr :: Expr -> Statement
 lexExpr (List []) = NullStatement
 lexExpr (Atom x) = Literal x
 lexExpr (List (x:xs)) = case (getAtomValuePartial x) of
-                          "=" -> case (getAtomValue (xs !! 0)) of
-                            (Just name) -> (Assignment name (lexExpr (xs !! 1)))
-                            (Nothing) -> (Error (SyntaxError "Cannot name variable using expression."))
-                          "defun" -> case (getAtomValue (xs !! 0)) of
-                            (Just name) -> (Defun name (lexExpr (xs !! 0)))
-                            (Nothing) -> (Error (SyntaxError "Cannot name function using expression."))
+                          "=" -> lexAssignment xs
+                          "defun" -> lexDefun xs
                           "type" -> TypeDeclaration (map lexExpr xs)
                           "$" -> Application (lexExpr (xs !! 0))
                           "+" -> Add (lexExpr (xs !! 0)) (lexExpr (xs !! 1))
@@ -33,4 +40,4 @@ lexExpr (List (x:xs)) = case (getAtomValuePartial x) of
                           "*" -> Multiply (lexExpr (xs !! 0)) (lexExpr (xs !! 1))
                           "/" -> Divide (lexExpr (xs !! 0)) (lexExpr (xs !! 1))
                           "^" -> Power (lexExpr (xs !! 0)) (lexExpr (xs !! 1))
-                          _ -> (Error (SyntaxError "Term not recongnized as a function, operator, or other known device."))                          
+                          _ -> (Error (SyntaxError "Term not recongnized as a function, operator, or other known device."))
